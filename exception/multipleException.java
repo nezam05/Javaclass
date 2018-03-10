d694a3d657c186f660bbda8fbbbf7d39798c7efd
nezam <nezam@thinkpool.net>
2018-03-10 18:31:05 +0600

    another test

--- exception/multipleException.java
+++ exception/multipleException.java
@@ -6,8 +6,8 @@ public class multipleException {
     int x[]={1,2,3,4,5};
     try {
         //System.out.println("x[6]"+x[6]);
-        System.out.println("x[3]: "+x[3]);
-        //x[3]=30/0;
+        // System.out.println("x[3]: "+x[3]);
+        x[3]=30/0;
         //String a= null;
         // if (a.equals("ggg")){
         //     System.out.println("Same");
